5f50bebce8fa6a42ffd6dc45c2a940edc05cb326
AleksMD <budjonniy291@gmail.com>
2019-09-26 13:06:15 +0300

    Roman_numerals_sum was modified. Added representation result as valid roman numerals

--- task#0/roman_numerals_sum.py
+++ task#0/roman_numerals_sum.py
@@ -3,7 +3,8 @@ import re
 stand = {'M': 1000, 'D': 500,
         'C': 100, 'L': 50,  'X': 10,
          'V': 5, 'I': 1}
-#This pattern was taken from the network and little bit modified
+complex_romans = {'CM': 900, 'CD': 400, 'XC': 90, 'XL': 40, 'IX': 9, 'IV': 4}
+#This pattern was taken from the web and little bit modified
 search_pattern = '^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|VI{0,3}|I{0,4}?)$'
 
 re_pattern = re.compile(search_pattern)
@@ -49,19 +50,42 @@ def roman_to_int_conv(num1: str) -> int:
 #x = roman_to_int_conv('MCMXL')
 #print(x)
 
-def roman_numerals_sum(num1: str, num2: str) -> int:
+def from_int_to_roman(num: int) -> 'str, roman numeral(X, V, I...)':
+
+    reverse_stand = {v: k for k, v in stand.items()}
+
+    reverse_stand.update({v: k for k, v in complex_romans.items()})
+
+    general = dict(sorted(reverse_stand.items(), key=lambda x: x[0], reverse=True))
+
+    roman = ''
+
+    for key in general.keys():
+        if num > key:
+            roman += num // key * general[key]
+            num = num % key
+        elif num == key:
+            roman += general[key]
+            break
+    return roman
+
+def roman_numerals_sum(num1: str, num2: str, roman_view=True) -> int:
     '''
     Calculates the sum of two roman numerals
 
     :return: the sum of two roman numerals as Integer
+    :roman_view=True, if it is False the result will be expressed in int format
     '''
 
     if isinstance(num1, str) and isinstance(num2, str):
-        return roman_to_int_conv(num1) + roman_to_int_conv(num2)
-
+        if roman_view:
+            return from_int_to_roman(roman_to_int_conv(num1) + roman_to_int_conv(num2))
+        else:
+            return roman_to_int_conv(num1) + roman_to_int_conv(num2)
     else:
         raise TypeError('Both arguments should be in the string format')
 
-#Uncomment following to test the summator:
-roman_sum = roman_numerals_sum('X', 'M')
-print(roman_sum)
+if __name__ == '__main__':
+    assert roman_numerals_sum('M', 'X') == 'MX'
+    assert roman_numerals_sum('V', 'V') == 'X'
+    
