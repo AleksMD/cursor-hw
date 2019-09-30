b4f785ea465845bb4f64255ea7cbe18b1972738a
AleksMD <budjonniy291@gmail.com>
2019-09-30 10:47:13 +0300

    Updated task 1

--- relational_database/homework.py
+++ relational_database/homework.py
@@ -26,6 +26,7 @@ def task_1_add_new_record_to_db(con) -> None:
     cursor.execute('''INSERT INTO customers(customername, contactname, 
                     address, city, postalcode, country) 
                     VALUES('Thomas', 'David', 'Some Address', 'London', '774', 'Singapore');''')
+    cursor.commit()
     cursor.close()
 
 
